1a5dc35bdfc495040ac3f9045cae8514fda01401
Arelia <arelia@github.com>
2020-04-11 21:26:00 -0500

    add user sign up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
     presence: true, 
     length: { minimum: 3, maximum: 25 }, 
     uniqueness: { case_sensitive: false }
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email,
     presence: true, 
     length: { maximum: 125 }, 
