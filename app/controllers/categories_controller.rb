8fabeb72b477851ab948d441a5d1353d100ffa26
Arelia <arelia@github.com>
2020-04-13 20:58:01 -0500

    updating categories controller plus tests

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,9 +4,26 @@ class CategoriesController < ApplicationController
   end
 
   def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "Category #{@category.name} was created successfully."
+      redirect_to categories_path
+    else
+      render 'new'
+    end
   end
 
   def show
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 
 end
